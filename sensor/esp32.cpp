14c943b3629694cb9760e689b5aac8fb187fbc2f
narapon <43005911+narapons@users.noreply.github.com>
2021-08-28 19:24:32 +0900

    Create esp32.cpp

---
+++ sensor/esp32.cpp
@@ -0,0 +1,14 @@
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  SerialBT.begin("Sensor");
+  pinMode(15,INPUT);
+}
+
+void loop() {
+  int sensor = digitalRead(15);
+  SerialBT.print(sensor);
+  delay(1000);
+}
