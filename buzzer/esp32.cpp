91165c4948871ff36dd780051ebb23d52cb9c6c3
narapon <43005911+narapons@users.noreply.github.com>
2021-09-14 09:32:36 +0900

    add LED

--- buzzer/esp32.cpp
+++ buzzer/esp32.cpp
@@ -2,19 +2,31 @@
 
 BluetoothSerial SerialBT;
 
+#define BZ_PIN 15
+#define LED_PIN 23
+
+void LED(){
+  digitalWrite(LED_PIN,HIGH);
+  delay(500);
+  digitalWrite(LED_PIN,LOW);
+  delay(500);
+}
+
 void setup() {
   SerialBT.begin("Buzzer");
-  pinMode(15,OUTPUT);
+  pinMode(BZ_PIN,OUTPUT);
+  pinMode(LED_PIN,OUTPUT);
 }
 
 void loop() {
   if (SerialBT.available()){
     int buzzer = digitalRead(15);;
     char blu = SerialBT.read();
-    if(blu == '1'){
-      digitalWrite(15,HIGH);
-    }else if(blu == '0'){
-      digitalWrite(15,LOW);
+    while(blu == '1'){
+       digitalWrite(BZ_PIN,HIGH);
+       LED();
     }
+    digitalWrite(BZ_PIN,LOW);
+    digitalWrite(LED_PIN,LOW);
   }
 }
