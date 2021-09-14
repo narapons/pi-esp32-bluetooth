e89d2c843afedf67159fb0ea53f4d93a2bb8cfa3
narapon <43005911+narapons@users.noreply.github.com>
2021-09-14 09:59:17 +0900

    add door

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import time
 
 serial_sensor = serial.Serial('/dev/rfcomm0')
 serial_buzzer = serial.Serial('/dev/rfcomm1')
+serial_door = serial.Serial('/dev/rfcomm2')
 
 while 1:
  print(serial_sensor.readline(1))
     
- if serial_sensor.readline(1) == b'1':
+ if serial_sensor.readline(1) == b'1' || serial_door.readline(1) == b'0':
     print(serial_buzzer.write(b'1'))
     time.sleep(3)
     print(serial_buzzer.write(b'0'))
