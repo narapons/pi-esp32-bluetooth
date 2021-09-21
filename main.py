379b18e965518e80128dc147cd0180f508980c84
narapon <43005911+narapons@users.noreply.github.com>
2021-09-21 12:48:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ serial_door = serial.Serial('/dev/rfcomm2')
 while 1:
  print(serial_sensor.readline(1))
     
- if serial_sensor.readline(1) == b'1' || serial_door.readline(1) == b'0':
+ if serial_sensor.readline(1) == b'1' or serial_door.readline(1) == b'0':
     print(serial_buzzer.write(b'1'))
     time.sleep(3)
-    print(serial_buzzer.write(b'0'))
-    time.sleep(1)
+ print(serial_buzzer.write(b'0'))
