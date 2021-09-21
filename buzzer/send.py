4c8eb4f05dd86041e78cd14998494d53ae275e20
narapon <43005911+narapons@users.noreply.github.com>
2021-09-21 21:34:45 +0900

    Update send.py

--- buzzer/send.py
+++ buzzer/send.py
@@ -1,8 +1,11 @@
+import sys
 import serial
 import time
 
 serial = serial.Serial('/dev/rfcomm1')
 
-while 1:
- print(serial.write(b'1'))
- break
+try:
+    while True:
+        print(serial.write(b'1'))
+except KeyboardInterrupt:
+    sys.exit(0)
