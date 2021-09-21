c32a31da1e51270537081376f8a71e75d350c89b
narapon <43005911+narapons@users.noreply.github.com>
2021-09-21 21:28:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import serial
 import time
 
@@ -5,10 +6,12 @@ serial_sensor = serial.Serial('/dev/rfcomm0')
 serial_buzzer = serial.Serial('/dev/rfcomm1')
 serial_door = serial.Serial('/dev/rfcomm2')
 
-while 1:
- print(serial_sensor.readline(1))
-    
- if serial_sensor.readline(1) == b'1' or serial_door.readline(1) == b'0':
-    print(serial_buzzer.write(b'1'))
-    time.sleep(3)
- print(serial_buzzer.write(b'0'))
+try:
+    while True:
+        print(serial_sensor.readline(1))
+        if serial_sensor.readline(1) == b'1' or serial_door.readline(1) == b'0':
+            print(serial_buzzer.write(b'1'))
+            time.sleep(3)
+            print(serial_buzzer.write(b'0'))
+except KeyboardInterrupt:
+    sys.exit(0)
