7a9b23ad12175e791fbde43a054a69e2084a9515
narapon <43005911+narapons@users.noreply.github.com>
2021-09-28 13:08:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ import collections as cl
 serial_sensor = serial.Serial('/dev/rfcomm0')
 serial_buzzer = serial.Serial('/dev/rfcomm1')
 serial_door = serial.Serial('/dev/rfcomm2')
+buzzer = 0
 
 try:
     while True:
-        sensor = int(serial_sensor.readline(1))
-        door = int(serial_door.readline(1))
-        buzzer = int(serial_buzzer.readline(1))
+        sensor = serial_sensor.readline(1)
+        door = serial_door.readline(1)
         data = cl.OrderedDict()
-        data["DATA"] = {"sendor":sensor, "door":door, "buzzer":buzzer}
-        with open('data.json', mode='w') as file:
+        data["DATA"] = {"sensor":sensor, "door":door, "buzzer":buzzer}
+        with open('data.json', mode='wt') as file:
             json.dump(data, file)
-        if serial_sensor.readline(1) == 1 or serial_door.readline(1) == 0:
+        print("checking...")
+        if serial_sensor.readline(1) == b'1' or serial_door.readline(1) == b'0':
             print(serial_buzzer.write(b'1'))
+            buzzer = 1
             time.sleep(3)
             print(serial_buzzer.write(b'0'))
+            buzzer = 0
 except KeyboardInterrupt:
     sys.exit(0)
