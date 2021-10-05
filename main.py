0526e7ec212eb6c117b28940ff015c9f3a6ad5e5
narapon <43005911+narapons@users.noreply.github.com>
2021-10-05 10:05:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import collections as cl
 serial_sensor = serial.Serial('/dev/rfcomm0')
 serial_buzzer = serial.Serial('/dev/rfcomm1')
 serial_door = serial.Serial('/dev/rfcomm2')
-buzzer = 0
+buzzer = "0"
 
 try:
     while True:
@@ -20,10 +20,10 @@ try:
         print("checking...")
         if sensor == b'1' or door == b'0':
             print(serial_buzzer.write(b'1'))
-            buzzer = 1
+            buzzer = "1"
             time.sleep(3)
         else:
             print(serial_buzzer.write(b'0'))
-            buzzer = 0
+            buzzer = "0"
 except KeyboardInterrupt:
     sys.exit(0)
