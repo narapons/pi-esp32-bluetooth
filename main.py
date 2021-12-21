a5d907d4ea4011987674f5b8158466abe95b864a
narapon <43005911+narapons@users.noreply.github.com>
2021-12-21 13:17:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,15 @@ buzzer = "0"
 
 try:
     while True:
+        print("checking...")
         sensor = serial_sensor.readline(1)
         door = serial_door.readline(1)
         data = cl.OrderedDict()
         data["DATA"] = [{"sensor":sensor, "door":door, "buzzer":buzzer}]
         with open('data.json', mode='wt') as file:
             json.dump(data, file)
-        print("checking...")
         if sensor == b'1' or door == b'0':
             print(serial_buzzer.write(b'1'))
             buzzer = "1"
-            time.sleep(3)
-        else:
-            print(serial_buzzer.write(b'0'))
-            buzzer = "0"
 except KeyboardInterrupt:
     sys.exit(0)
